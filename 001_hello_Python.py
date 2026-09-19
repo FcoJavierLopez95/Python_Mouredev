70aace9a79c9ef6d2a944aeb4950fe33215797a0
Javier López Arce <javier.lopez@solvam.es>
2026-09-19 17:22:59 +0200

    .

---
+++ 001_hello_Python.py
@@ -0,0 +1,28 @@
+# Hola mundo
+print ("Hola Python")
+print ('Hola Python')
+
+###Curso
+    ## https://github.com/mouredev/hello-python
+
+### Documentación
+    ## https://www.python.org/
+
+### Sobre python:    
+    ## Lenguaje de programación de alto nivel, 
+    ## multiparadigma: Permite usar múltiples estilos de programación según convenga, en lugar de obligarte a seguir uno solo (Funcional, POO, Procedimental...). 
+    ## Interpretado: El intérprete (CPython) traduce el código a un formato intermedio (bytecode) y lo ejecuta en tiempo real, sin requerir una compilación previa a binario ejecutable.
+    ## Tipado Dinámico: No se declaran los tipos de las variables; el tipo se determina automáticamente durante la ejecución según el valor asignado.
+    ## Tipado Fuerte: No realiza conversiones implícitas de tipos incompatibles (por ejemplo, "10" + 5 genera un TypeError en lugar de convertir el número a texto).
+
+#BUENAS PRÁCTICAS:
+    ## La identación es de 4 espacios por nivel de sangría.
+    ## Python usa indistintamente comillas dobles o simples.
+    ## Anotaciones de tipo (Type Hints): Indicar el tipo de dato a la variable. (nombre: str = "")
+        ### Esto no obligará a que se cumplan en tiempo de ejecución ni lanzará un error si pasas un tipo distinto, solo ayuda a mejorar la documentación y la detección de errores.
+
+    ## Convención de Nombres:
+        ### snake_case: Para funciones y variables (mi_variable, obtener_datos()).
+        ### PascalCase: Para clases (MiClase).
+        ### UPPER_CASE: Para constantes (VALOR_MAXIMO).
+
---
+++ 002_tipos_De_Datos.py
@@ -0,0 +1,45 @@
+# Esto es un comentario En una línea
+
+"""
+Este es un 
+comentario 
+en varias líneas
+
+"""
+
+'''
+Este también
+ es un comentario 
+en varias líneas
+
+'''
+
+"""
+Python es un lenguaje de tipo dinámico por lo que 
+no hace falta indicar el tipo de dato, 
+Es buena práctica introducirrlo para 
+ayudar a mejorar la documentación y la detección de errores.
+
+"""
+
+### Variables (snake_case)
+
+## Cadenas de texto:
+my_string: str= ("Esto es un string o cadena de texto") 
+print (type (my_string))
+
+## Números enteros:
+my_integer: int= 1         
+print (type (my_integer))
+
+## Números con decimales:
+my_float: float= 1.5
+print (type (my_float))
+
+## Booleanos (True o False):
+my_boolean: bool=True
+print (type (my_boolean))
+
+## Arrays o Listas
+my_array: list= [1, 3, 7, 9, "gato", my_float]
+print (type (my_array))
