34bfc6a4e5d92d69a19834f5e694d6a30b3505ab
Javier López Arce <javier.lopez@solvam.es>
2026-09-20 13:00:54 +0200

    Se añade strings.py

---
+++ 003_strings.py
@@ -0,0 +1,59 @@
+### Strings
+
+my_string = "Mi String"
+my_other_string = 'Mi otro String'
+
+print(len(my_string))
+print(len(my_other_string))
+print(my_string + " " + my_other_string)
+
+my_new_line_string = "Este es un String\ncon salto de línea"
+print(my_new_line_string)
+
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string)
+
+my_scape_string = "\\tEste es un String \\n escapado"
+print(my_scape_string)
+
+## Formateo
+name, surname, age = "Juan", "Valdes", 30
+print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
+print("Mi nombre es %s %s y mi edad es %d" % (name, surname, age))              ## Esta es la mejoropción para cambiar el tipo de dato
+print("Mi nombre es " + name + " " + surname + " y mi edad es " + str(age))
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")                      ## Esta es la mejoropción si no quieres cambiar el tipo de dato.
+
+## Desempaqueado de caracteres
+language = "python"
+a, b, c, d, e, f = language
+print(a)
+print(e)
+
+#· División
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+language_slice = language[0:6:2]
+print(language_slice)
+
+# Reverse
+reversed_language = language[::-1]
+print(reversed_language)
+
+# Funciones del lenguaje
+print(language.capitalize())        # Hace que empiece por mayuscula
+print(language.upper())             # Cambia todo a mayus 
+print(language.lower())             # Cambia todo a minus
+print(language.count("t"))          # Cuenta el número de elementos incluyendo caracteres
+print(language.isnumeric())         # Pregunta si es numérico (True o false)
+print("1".isnumeric())              # Ojo prefunta si es numérico, no si es de tipo numero. (float, intefer etc)
+
+print(language.lower().isupper())   # Pregunta si esta en mayus
+print(language.startswith("Py"))    # Empieza por? ("X") distingue mayus de minus
+print("Py" == "py")                 # No es lo mismo
